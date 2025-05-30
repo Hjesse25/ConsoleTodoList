f6432a279fa200bb4c448add3a3826f5fa1b36e4
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-05-29 19:32:38 -0700

    add menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Menu();
+
+void Menu()
+{
+    bool isMenu = true;
+    do
+    {
+        Console.WriteLine("Todo List App");
+        Console.WriteLine("1. Add task");
+        Console.WriteLine("2. Remove task");
+        Console.WriteLine("3. View tasks");
+        Console.WriteLine("4. Exit");
+
+        try
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    isMenu = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid number.");
+                    continue;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Input must be in a correct format");
+        }
+        finally
+        {
+            Console.WriteLine();
+        }
+    } while (isMenu);
+}
